1e5d6743633cff6829d31b7472da07b287c26ad6
carlostorresfiuni19999 <carlos.torres@fiuni.edu.py>
2022-04-06 22:55:46 -0400

    Actualizado el diseño de la base de datos, Configurado los Connections Strings en .net, y tablas de usuarios y roles

--- Back-End/cuentasctacte-web-api/Models/IdentityModels.cs
+++ Back-End/cuentasctacte-web-api/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace cuentasctacte_web_api.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("VentasYCuentasCorrientes", throwIfV1Schema: false)
         {
         }
         

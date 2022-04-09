47abad0436225338a3cccde4ad38d8fb475ee212
carlostorresfiuni19999 <carlos.torres@fiuni.edu.py>
2022-04-09 15:06:25 -0400

    Creado la tabla Depositos

---
+++ Back-End/cuentasctacte-web-api/Models/Entities/Deposito.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace cuentasctacte_web_api.Models.Entities
+{
+    public class Deposito : Data
+    {
+        public int Id { get; set; }
+        public string NombreDeposito { get; set; }
+        public string DescripcionDeposito { get; set; }
+
+    }
+}

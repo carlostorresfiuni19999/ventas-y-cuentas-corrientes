562acef560cd6f91d8f7aac4ae8ff2bb710f4293
carlostorresfiuni19999 <carlos.torres@fiuni.edu.py>
2022-04-09 09:38:33 -0400

    Configurado origenes crusados

--- Back-End/cuentasctacte-web-api/Startup.cs
+++ Back-End/cuentasctacte-web-api/Startup.cs
@@ -4,14 +4,17 @@ using System.Linq;
 using Microsoft.Owin;
 using Owin;
 
-[assembly: OwinStartup(typeof(cuentasctacte_web_api.Startup))]
 
+[assembly: OwinStartup(typeof(cuentasctacte_web_api.Startup))]
 namespace cuentasctacte_web_api
 {
+
+
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
             ConfigureAuth(app);
         }
     }

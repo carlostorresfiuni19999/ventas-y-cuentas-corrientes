aeb7ea7c7dfd17facd25a2e7cd6aea01e3cf9ba4
carlostorresfiuni19999 <carlos.torres@fiuni.edu.py>
2022-04-24 19:50:02 -0400

    Mejorado el filtrado de delete. Listar por Productos implementado

--- Front-End/ventasctacte/components/LoginForm.js
+++ Front-End/ventasctacte/components/LoginForm.js
@@ -1,53 +1,108 @@
 import React from 'react'
-import {Formik, Form} from "formik";
+import { useFormik } from "formik";
 import * as Yup from "Yup";
-import FormikControl from "./FormikControl"
 import styles from '../styles/LogIn.module.css'
+import login from "../API/login"
+import { useRouter } from 'next/router';
 
-function LoginForm(){
-    const initialValues ={
-        user: '',
-        pass: ''
-    }
+function LoginForm() {
+    const router = useRouter()
+    const formik = useFormik({
+        initialValues: {
+            user: '',
+            pass: ''
+        },
+        onSubmit: (values) => {
+            console.log("Hola mundo")
+            login(values.user, values.pass)
+            .catch(error => {
+                console.log('error', error);
+                alert("Error al intentar ingresar. Verifica las credenciales");
+            })
+                .then(response => response.text())
+                .then(result => {
+                    console.log(result);
+                    const res = JSON.parse(result)
 
-    const validacion = Yup.object({
-        user: Yup.string().required('*Obligatorio'),
-        //pass: Yup.string().required('*Obligatorio').matches(/^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
-        //    "La contraseña debe contener al menos 8 letras, uno Mayuscula un numero y una figura")
-        pass: Yup.string().required('*Obligatorio')
+                    if(res.error_description){
+                        alert("Error, Credenciales no validas");
+                    }else{
+                        sessionStorage.setItem("token", JSON.stringify(res));
+                        alert("Logeado con exito");
+                        router.push("ndp/lista")
+                    }
+                    
+                    
+                }
+                )
+
+        },
+        validacion: Yup.object({
+            user: Yup.string().required('*Obligatorio'),
+            //pass: Yup.string().required('*Obligatorio').matches(/^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
+            //    "La contraseña debe contener al menos 8 letras, uno Mayuscula un numero y una figura")
+            pass: Yup.string().required('*Obligatorio')
+        }
+        )
     })
 
-    //Se debe cambiar a el submit del BDO
-    const onSubmit = values =>{
-        console.log('Form data', values)
-    }
+    return (
 
-    return(
-        <div className = {styles.LoginPanel}>
+
+        /*  <div className={styles.LoginPanel}>
+             <h1 className={styles.letraGrande}>Login</h1>
+             
+             
+                 formik => {
+                     return <Form onSubmit={formik.handleSubmit}>
+                         <FormikControl
+                             control='input'
+                             type='text'
+                             label="Usuario"
+                             name='user'
+ 
+                         />
+                         <FormikControl
+                             control='input'
+                             type='password'
+                             label="Contraseña"
+                             name='pass'
+                         />
+                         <button type="submit" className={styles.boton} disabled={!formik.isValid}> Login </button>
+                     </Form>
+                 }
+             }
+         </Formik>
+         </div >
+         */
+
+        <div className={styles.LoginPanel}>
             <h1 className={styles.letraGrande}>Login</h1>
-            <Formik initialValues={initialValues} validationSchema = {validacion} onSubmit={onSubmit}>
-                {
-                    formik => {
-                        return <Form>
-                            <FormikControl
-                                control = 'input'
-                                type = 'text'
-                                label = "Usuario"
-                                name = 'user'
-                            />
-                            <FormikControl
-                                control = 'input'
-                                type = 'password'
-                                label = "Contraseña"
-                                name = 'pass'
-                            />
-                            <button type="submit" className={styles.boton} disabled = {!formik.isValid}> Login </button>
-                        </Form>
-                    }
-                }
-            </Formik>
+            <form onSubmit={formik.handleSubmit}>
+                <label htmlFor='user'>
+                    Email
+                </label>
+                <input
+                    id="user"
+                    name="user"
+                    type="email"
+                    onChange={formik.handleChange}
+                    value={formik.values.user}
+                />
+                <label htmlFor='password'>
+                    Password
+                </label>
+
+                <input
+                    id="pass"
+                    name="pass"
+                    type="password"
+                    onChange={formik.handleChange}
+                    value={formik.values.pass}
+                />
+                <button type="submit" className={styles.boton} disabled={!formik.isValid}> Login </button>
+            </form>
         </div>
-        
     );
 }
 

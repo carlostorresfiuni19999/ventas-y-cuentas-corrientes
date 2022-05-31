c5908a1da1d44307d63f8c498ab5433dc63d0819
KaitoK1kuchi <101877459+KaitoK1kuchi@users.noreply.github.com>
2022-05-31 01:31:47 -0400

    commit de la front end, facturas, veridficaciones

--- Front-End/ventasctacte/components/LoginForm.js
+++ Front-End/ventasctacte/components/LoginForm.js
@@ -4,6 +4,7 @@ import * as Yup from "yup";
 import styles from '../styles/LogIn.module.css'
 import login from "../API/login"
 import { useRouter } from 'next/router';
+import hasRole from '../API/hasRole';
 
 function LoginForm() {
     const router = useRouter()
@@ -27,6 +28,7 @@ function LoginForm() {
                     if(res.error_description){
                         alert("Error, Credenciales no validas");
                     }else{
+                        hasRole(res.access_token, res.userName, "Cajero");
                         sessionStorage.setItem("token", JSON.stringify(res));
                         alert("Logeado con exito");
                         router.push("ndp/Lista")
--- Front-End/ventasctacte/pages/factura/detalles/[id].js
+++ Front-End/ventasctacte/pages/factura/detalles/[id].js
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react'
 
 //next
-import Router from 'next/router'
+import { useRouter } from 'next/router'
 import Head from 'next/head'
 import Link from 'next/link'
 
@@ -11,7 +11,78 @@ import Link from 'next/link'
 import Navbar from '../../../components/Navbar'
 import MDPControl from '../../../components/MDPControl'
 
+//API
+import getFactura from '../../../API/getFactura'
+
 export default function detalles() {
+
+    const [factura, setFactura] = useState({
+        cliente: "",
+        cin: 0,
+        fechaFact: 'YYYY-MM-DD',
+        detalles: [],
+        cuotas: []
+    })
+    const [keyCounter, setKeyCounter] = useState(0)
+    const [keyCuota, setKeyCuota] = useState(0)
+    const router = useRouter()
+
+    useEffect(() => {
+        if (router.isReady) {
+            getFactura(JSON.parse(sessionStorage.getItem('token')).access_token, router.query.id)
+                .then(response => response.text())
+                .then(result => {
+                    const res = JSON.parse(result)
+                    console.log(res)
+                    setFactura({
+                        cliente: res.Cliente,
+                        cin: parseInt(res.DocCliente),
+                        fechaFact: res.FechaFacturacion.split('T')[0],
+                        precioTotal: res.PrecioTotal + res.IvaTotal,
+                        saldoTotal: res.SaldoTotal,
+                        cuotas: res.Cuotas.map(cuota => {
+                            const returnValue = {
+                                fechaVenc: cuota.FechaVencimiento.split('T')[0],
+                                monto: cuota.Monto,
+                                saldo: cuota.Saldo,
+                                key: cuota.Id
+                            }
+                            return returnValue
+                        }),
+                        detalles: res.Detalles.map(detalle => {
+                            const returnValue = {
+                                cantidad: detalle.Cantidad,
+                                nombreProd: detalle.Producto,
+                                precioUnit: detalle.PrecioUnitario + detalle.Iva,
+                                precioTotal: (detalle.PrecioUnitario + detalle.Iva) * detalle.Cantidad,
+                                key: detalle.Id
+                            }
+                            return returnValue
+                        })
+                    })
+
+
+                }).catch(err => console.log(err))
+
+
+        }
+
+    }, [router.isReady])
+
+    console.log(factura)
+
+    const formatfecha = (dateStr) => {
+        if (dateStr == null) {
+            return ' '
+        }
+        const dArr = dateStr.split("-");  // ex input "2010-01-18"
+        return dArr[2] + "/" + dArr[1] + "/" + dArr[0].substring(2); //ex out: "18/01/10"
+    }
+
+    const formatNum = (data) => {
+        return new Intl.NumberFormat('us-US', { style: 'decimal', currency: 'PGS' }).format(data)
+    }
+
     return (
         <div>
             <Head>
@@ -25,11 +96,9 @@ export default function detalles() {
                 {/*La parte de arriba*/}
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
                     <div className='ms-4'>
-                        <Link href='/' >
-                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-arrow-left-short" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-arrow-left-short" viewBox="0 0 16 16" onClick={() => { router.back() }}>
                                 <path fillRule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z" />
                             </svg>
-                        </Link>
                     </div>
 
 
@@ -57,15 +126,13 @@ export default function detalles() {
                 <div className='ps-5 py-2'>
                     <div>
                         <label className='ps-2'>Cliente:</label>
-                        <label className='px-3'></label>
+                        <label className='px-3'>{factura.cliente}</label>
                         <label className=''>CIN:</label>
-                        <label className='px-3 pe-5'></label>
+                        <label className='px-3 pe-5'>{formatNum(factura.cin)}</label>
                     </div>
                     <div>
-                        <label className='ps-2'>Fecha:</label>
-                        <label className='px-3'></label>
-                        <label className=''>Vendedor:</label>
-                        <label className='px-3 pe-5'></label>
+                        <label className='ps-2'>Fecha Facturada:</label>
+                        <label className='px-3'>{formatfecha(factura.fechaFact)}</label>
                     </div>
                 </div>
             </div>
@@ -74,84 +141,66 @@ export default function detalles() {
                     <thead className='thead-dark'>
                         <tr>
                             <th scope='col'>Producto</th>
-                            <th scope='col'>Cod. Barra</th>
                             <th scope='col'>Cantidad</th>
-                            <th scope='col'>Cantidad Facturada</th>
-                            <th scope='col'>Saldo</th>
+                            <th scope='col'>Precio Unitario</th>
                             <th scope='col'>Precio Total</th>
                             <th scope='col'> </th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th>Intel -----</th>
-                            <td>123456789</td>
-                            <td>2</td>
-                            <td>1</td>
-                            <td>500.000</td>
-                            <td>1.000.000</td>
-                            <td></td>
-                        </tr>
+                        {
+                            factura.detalles.map(detalle => {
+                                return (
+                                    <tr key={detalle.key}>
+                                        <th>{detalle.nombreProd}</th>
+                                        <td>{detalle.cantidad}</td>
+                                        <td>{formatNum(detalle.precioUnit)}</td>
+                                        <td>{formatNum(detalle.precioTotal)}</td>
+                                        <td></td>
+                                    </tr>
+                                )
+                            })
+                        }
+
 
 
 
                     </tbody>
 
                 </table>
-                <div>
-                    <h6 className='float-end pe-5'>1.000.000 Gs</h6>
-                    <h6 className='float-end pe-2'>Total:</h6>
-                    <h6 className='float-end pe-5'>500.000</h6>
-                    <h6 className='float-end pe-2'>Saldo:</h6>
-
-                </div>
             </div>
 
-            <div className='mt-5 pt-5 container'>
-                <div className='row'>
-                    <div className='col-8'>
-                        <h5 className='pt-2'>Pagos Realizados</h5>
-                        <div className='pt-4'>
-                            <table className='table'>
-                                <thead className='thead-dark'>
-                                    <tr>
-                                        <th scope='col'>Fecha</th>
-                                        <th scope='col'>metodo de pago</th>
-                                        <th scope='col'>Saldo</th>
-                                        <th scope='col'>Monto de pago</th>
-                                        <th scope='col'>Total Pagado</th>
+            <div className=' pt-5 mx-5 px-4'>
+                <div className=''>
+                    <h5 className='pt-2'>Cuotas</h5>
+                    <div className='pt-4'>
+                        <table className='table'>
+                            <thead className='thead-dark'>
+                                <tr>
+                                    <th scope='col'>Fecha Vencimiento</th>
+                                    <th scope='col'>Monto Pago</th>
+                                    <th scope='col'>Saldo</th>
+                                    <th>PAGAR</th>
 
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <th>DD/MM/YY</th>
-                                        <td>Efectivo</td>
-                                        <td>500.000 Gs</td>
-                                        <td>500.000 Gs</td>
-                                        <td>500.000 Gs</td>
-                                    </tr>
-                                    <tr>
-                                        <th>DD/MM/YY</th>
-                                        <td>Efectivo</td>
-                                        <td>200.000 Gs</td>
-                                        <td>300.000 Gs</td>
-                                        <td>800.000 Gs</td>
-                                    </tr>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    factura.cuotas.map(cuota => {
+                                        return (
+                                            <tr key={cuota.key}>
+                                                <th>{formatfecha(cuota.fechaVenc)}</th>
+                                                <td>{formatNum(cuota.monto)}</td>
+                                                <td>{formatNum(cuota.saldo)}</td>
+                                                <td><button className='btn btn-primary btn-sm' onClick={()=>{}}> Pagar</button></td>
+                                            </tr>
+                                        )
+                                    })
+                                }
 
+                            </tbody>
 
-                                </tbody>
-
-                            </table>
-                            <div>
-                                <h6>Descripcion:</h6>
-                                <label></label>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='col-4'>
-                        <h5>Pagar</h5>
-                        <MDPControl  mdp = 'EFECTIVO'  montoMax={200000} />
+                        </table>
                     </div>
                 </div>
             </div>

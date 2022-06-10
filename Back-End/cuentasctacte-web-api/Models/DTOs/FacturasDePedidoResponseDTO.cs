2f5e323568be8088da73ed5003169b2833d85d4f
enrique <enrique.rios2019@fiuni.edu.py>
2022-06-10 14:49:28 -0400

    Agregacion Facturas de pedido response DTO

---
+++ Back-End/cuentasctacte-web-api/Models/DTOs/FacturasDePedidoResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cuentasctacte_web_api.Models.DTOs
+{
+    public class FacturasDePedidoResponseDTO
+    {
+        
+        public PedidoDetalleResponseDTO PedidoDetalleResponseDTO { get; set; }
+        public List<FacturaDetalleResponseDTO> FacturasDetalles { get; set; }
+
+    }
+}
